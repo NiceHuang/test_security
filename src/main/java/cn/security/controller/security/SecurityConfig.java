f6542ef419eca433a53d1108d42e11544db932c3
NiceHuang <huang2275308328@163.com>
2018-08-09 00:13:31 +0800

    1、自定义认证

--- src/main/java/cn/security/controller/security/SecurityConfig.java
+++ src/main/java/cn/security/controller/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package cn.security.controller.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,9 +21,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private SuccessHandler successHandler;
 
+    @Autowired
+    private MyAuthenticationProvider provider;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
+        http.authorizeRequests()
+                .anyRequest().authenticated()
                 .and().formLogin().loginPage("/hello").loginProcessingUrl("/login").permitAll()
                 .failureHandler(failureHandler).successHandler(successHandler)
                 .and().logout().permitAll()
@@ -32,5 +37,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .authorizeRequests()    // 定义哪些URL需要被保护、哪些不需要被保护
 //                .anyRequest()        // 任何请求,登录后可以访问
 //                .authenticated();
+    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        //将验证过程交给自定义验证工具
+        auth.authenticationProvider(provider);
     }
 }
